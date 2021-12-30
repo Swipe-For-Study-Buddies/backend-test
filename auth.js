7d0348c87e4eabbd5b7a02f405e711728201bcbe
ettoolong <ettoolong@hotmail.com>
2021-12-30 21:55:56 +0800

    initial commit

---
+++ auth.js
@@ -0,0 +1,155 @@
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const { encrypt, decrypt } = require('./crypto');
+
+function genRandomString(length) {
+  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+};
+
+function sha512(password, salt) {
+  const hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  return hash.digest('hex');
+};
+
+function saltHashPassword(password) {
+  const salt = genRandomString(16);
+  const passwordHash = sha512(password, salt);
+  return {
+    salt,
+    passwordHash
+  }
+}
+
+function getToken(email) {
+  return jwt.sign({ email, timestamp: Date.now() }, 'secret', { algorithm: 'HS256' });
+}
+
+const register = async function ({ req, res, db }) {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).send({})
+  }
+
+  // 要先確認這個 email 沒有註冊過
+  let emailExist = true
+  db.get(email, (err, data) => {
+    if (err && err.type === 'NotFoundError') {
+      emailExist = false
+    }
+  })
+
+  if (emailExist) {
+    return res.status(400).send({ message: 'account already exist' })
+  }
+
+  const { salt, passwordHash } = saltHashPassword(password);
+  await db.put(email, JSON.stringify({ salt, passwordHash }))
+  // NOTE: 此處應該不要直接給出 JWT, 應該先寄一個啟用帳號連結的 email
+  //       但是因為這只是一個測試串接用的後端, 所以就不在這邊實作這一段
+
+  // 產生 JWT, 丟回 clent 端
+  const token = getToken(email)
+  return res.json({ token })
+}
+
+const login = async function ({ req, res, db }) {
+  const { email, password } = req.body
+  try {
+    const data = await db.get(email)
+    const jsonData = JSON.parse(data)
+    const { salt, passwordHash, ...userData } = jsonData
+    const passwordHashFromClient = sha512(password, salt);
+    if (passwordHash === passwordHashFromClient) {
+      const token = getToken(email)
+      return res.json({ ...userData, token })
+    } else {
+      return res.status(401).send({ message: 'WrongPassword' });
+    }
+  } catch (err) {
+    if (err.type === 'NotFoundError') {
+      return res.status(401).send({ message: 'UserNotFound' });
+    }
+  }
+}
+
+const getResetPasswordToken = async function ({ req, res, db }) {
+  const { email } = req.body
+  console.log(email)
+  try {
+    const data = await db.get(email)
+    const jsonData = JSON.parse(data)
+    const rpToken = genRandomString(16)
+    const rptExpired = Date.now() + 60 * 60 * 24 * 2 * 1000 // 2天過期
+    await db.put(email, JSON.stringify({ ...jsonData, rpToken, rptExpired }))
+
+    const hash = encrypt(`${email}:${rpToken}`);
+    // NOTE: 此處應該寄一封 email 給使用者, 附上重設密碼用的連結
+    //       目前先把連結印出來手動在瀏覽器輸入
+    console.log(`http://localhost:3000/resetPassword/${hash}`)
+    return res.status(200).send({ message: 'OK' });
+  } catch (err) {
+    console.log(err)
+    if (err.type === 'NotFoundError') {
+      return res.status(401).send({ message: 'UserNotFound' });
+    }
+  }
+}
+
+const resetPassword = async function ({ req, res, db }) {
+  const { password, token } = req.body
+  const content = decrypt(token)
+  const s = content.split(':')
+  if (s.length !== 2) {
+    return res.status(400).send({ message: 'InvalidToken' });
+  } else {
+    const email = s[0]
+    const rpToken = s[1]
+
+    try {
+      const data = await db.get(email)
+      const jsonData = JSON.parse(data)
+      const { rpToken: rpTokenInDb, rptExpired, ...userData } = jsonData
+      // 把收到的 rpToken 和資料庫裡的 token 比對, 並確認 token 尚未過期
+
+      if (rpToken !== rpTokenInDb) {
+        return res.status(400).send({ message: 'InvalidToken' });
+      } else if (rptExpired < Date.now()) {
+        return res.status(400).send({ message: 'TokenExpired' });
+      }
+      if (password) {
+        // 更新 db 裡的 salt 和 password hash
+        const { salt, passwordHash } = saltHashPassword(password);
+        await db.put(email, JSON.stringify({ ...userData, salt, passwordHash }))
+      }
+      return res.status(200).send({ message: 'OK' });
+    } catch (err) {
+      console.log(err)
+      return res.status(400).send({ message: 'InvalidToken' });
+    }
+  }
+}
+
+// NOTE: 測試用的, 後端無須實作
+const me = async function ({ req, res, db }) {
+  const { email } = req.user
+  try {
+    const data = await db.get(email)
+    const jsonData = JSON.parse(data)
+    const { salt, passwordHash, ...userData } = jsonData
+    return res.json(userData)
+  } catch (err) {
+    if (err.type === 'NotFoundError') {
+      return res.status(401).send({ message: 'UnauthorizedError' });
+    }
+  }
+}
+
+module.exports = {
+  register,
+  login,
+  getResetPasswordToken,
+  resetPassword,
+  me
+}
+
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const cors = require('cors')
+const expressJWT = require('express-jwt')
+const level = require('level')
+const {
+  register,
+  login,
+  getResetPasswordToken,
+  verifyResetPasswordToken,
+  resetPassword,
+  me
+} = require('./auth.js')
+
+const db = level('db')
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressJWT({
+  secret: 'secret',  // 簽名的密鑰, 之後要放到 .env
+  algorithms: ['HS256'],
+}).unless({
+  path: [
+    '/api/auth/register',
+    '/api/auth/login',
+    '/api/auth/getResetPasswordToken',
+    '/api/auth/resetPassword',
+    '/api/auth/verifyResetPasswordToken',
+  ]
+}))
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(err.status).send({message: err.message});
+    return;
+  }
+  next();
+});
+
+app.post('/api/auth/register', async (req, res) => {
+  return register({req, res, db})
+});
+
+app.post('/api/auth/login', async (req, res) => {
+  return login({req, res, db})
+});
+
+app.post('/api/auth/getResetPasswordToken', async (req, res) => {
+  return getResetPasswordToken({req, res, db})
+});
+
+app.post('/api/auth/resetPassword', async (req, res) => {
+  return resetPassword({req, res, db})
+});
+
+app.post('/api/auth/verifyResetPasswordToken', async (req, res) => {
+  return resetPassword({req, res, db})
+});
+
+app.get('/api/auth/me', async (req, res) => {
+  return me({req, res, db})
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send()
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+});
